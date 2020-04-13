70f2e52018b97a40476ef73b06252ffcc56ad19c
Zak Laughton <zlaughton@gmail.com>
2020-04-12 21:38:51 -0500

    shrink field

--- src/context/gameReducer.js
+++ src/context/gameReducer.js
@@ -4,8 +4,8 @@ export const initialState = {
   day: 0,
   people: [],
   historicalInfectedCount: [{ day: 0, count: 0 }],
-  gridSize: 25,
-  boardSize: 700,
+  gridSize: 19,
+  boardSize: 500,
   peopleDensity: 0.3,
   topOfTheCurve: 0,
 }
